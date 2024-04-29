3aaaf3f093b6004ade83de76df7aec240a008daf
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-04-29 22:17:12 +0300

    added index.php

---
+++ cms/index.php
@@ -0,0 +1,15 @@
+<?php
+spl_autoload_register(static function ($className){
+    $path = str_replace('\\', '/', $className.'.php');
+
+    if(file_exists($path))
+        include_once ($path);
+});
+
+if(isset($_GET['route']))
+    $route = $_GET['route'];
+else
+    $route = '';
+
+$router = new core\Router($route);
+$router->run();
