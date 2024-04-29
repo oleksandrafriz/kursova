9cbc4052160eeff1d0bb825fc6f392c0c0c3a25d
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-04-30 01:09:29 +0300

    added Template

---
+++ cms/core/Template.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace core;
+
+class Template
+{
+    protected $templateFilePath;
+    protected $paramsArray;
+    public function __construct($templateFilePath)
+    {
+        $this->templateFilePath = $templateFilePath;
+        $this->paramsArray = [];
+    }
+    public function setParam($paramName, $paramValue)
+    {
+        $this->paramsArray[$paramName] = $paramValue;
+    }
+
+    public function setParams($params)
+    {
+        foreach ($params as $key => $value)
+            $this->setParam($key, $value);
+    }
+
+    public function getHTML(){
+        ob_start();
+        /*
+         * $this->paramsArray = [
+             * 'Content' => 'контент сайту',
+             * 'Title' => 'заголовок сайту'
+         * ];
+         *
+         * $Content = 'контент сайту';
+         * $Title = 'заголовок сайту';
+         * */
+        extract($this->paramsArray);
+        include ($this->templateFilePath);
+        $str = ob_get_contents();
+        ob_end_clean();
+        return $str;
+    }
+
+    public function display()
+    {
+        echo $this->getHTML();
+    }
+}
