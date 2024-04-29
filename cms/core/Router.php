d626895c8f267092d738800e7b6473ad0c97a5de
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-04-30 01:08:18 +0300

    edited Router

--- cms/core/Router.php
+++ cms/core/Router.php
@@ -5,9 +5,12 @@ namespace core;
 class Router
 {
     protected $route;
+    protected $indexTemplate;
     public function __construct($route)
     {
         $this->route = $route;
+        $this->indexTemplate = new \core\Template('views/layouts/index.php');
+
     }
     public function run()
     {
@@ -27,16 +30,21 @@ class Router
             $controllerObject = new $controller();
             if(method_exists($controller, $method)){
                 array_splice($parts, 0, 2);
-                $controllerObject -> $method($parts);
-            }
-
-            else
+               $params = $controllerObject -> $method($parts);
+               $this->indexTemplate->setParams($params);
+            }else
                 $this->error(404);
         }else
             $this->error(404);
 
 
     }
+
+    public function done()
+    {
+        $this->indexTemplate->display();
+    }
+
     public function error($code)
     {
         http_response_code($code);
