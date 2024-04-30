a3a431d9ccdda0f9353f5d8a41bada4b262c94a8
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-04-30 13:27:48 +0300

    added changes for SiteController

--- cms/controllers/SiteController.php
+++ cms/controllers/SiteController.php
@@ -2,10 +2,13 @@
 
 namespace controllers;
 
-class SiteController
+use core\Controller;
+use core\Template;
+
+class SiteController extends Controller
 {
     public function actionIndex(){
-    echo 'main page';
+        return $this->render();
     }
     public function actionError($code){
         echo $code;
