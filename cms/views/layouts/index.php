7487bf64fbaca956d600af0185869f1ad6dbf731
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-04-30 13:29:26 +0300

    added title for layouts/index.php

--- cms/views/layouts/index.php
+++ cms/views/layouts/index.php
@@ -1,3 +1,11 @@
+<?php
+/** @var string $Title */
+/** @var string $Content */
+if(empty($Title))
+    $Title = '';
+?>
+
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,7 +13,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?=$Title ?></title>
+    <title><?= $Title ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"/>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -55,7 +63,7 @@
 
     <div>
         <h1><?=$Title?></h1>
-        <?=$Content?>
+        <?= $Content?>
     </div>
 
     <footer class="py-3 my-4">
