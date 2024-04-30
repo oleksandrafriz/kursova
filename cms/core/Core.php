2059a3b77fabf1beb52fedf1659c58448628ac4b
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-04-30 13:31:36 +0300

    added Core

---
+++ cms/core/Core.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace core;
+
+class Core
+{
+    public $defaultLayoutPath = 'views/layouts/index.php';
+    public $moduleName;
+    public $actionName;
+    public $router;
+    public $template;
+    private static $instance;
+
+    private function __construct(){
+        $this->template = new Template($this->defaultLayoutPath);
+    }
+
+    public function run($route){
+        $this->router = new \core\Router($route);
+        $params = $this->router->run();
+        $this->template->setParams($params);
+    }
+
+    public function done(){
+        $this->template->display();
+        $this->router->done();
+    }
+
+    public static function get(){
+        if(empty(self::$instance))
+            self::$instance = new Core();
+        return self::$instance;
+    }
+}
