e26c571d2fe1310fa22ebd66871a772e4768c5ed
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-04-30 18:43:02 +0300

    created config files

---
+++ cms/config/config.php
@@ -0,0 +1,5 @@
+<?php
+$Config[] = [
+    'title' => 'Приклад написання CMS-системи',
+    'adminEmail' => 'admin@cms.com'
+];
---
+++ cms/config/database.php
@@ -0,0 +1,7 @@
+<?php
+$Config[] = [
+    'dbHost' => 'localhost',
+    'dbName' => 'cms',
+    'dbLogin' => 'cms',
+    'dbPassword' => '@pC4/QJ0owW1.CKc',
+];
