d3b60e7ab43f8aed3c65c7d132bf1966f6e66b23
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-05-02 21:03:20 +0300

    added changes for UsersController

--- cms/controllers/UsersController.php
+++ cms/controllers/UsersController.php
@@ -2,10 +2,30 @@
 
 namespace controllers;
 
-class UsersController
+use core\Controller;
+use core\Core;
+use models\Users;
+
+class UsersController extends Controller
 {
-    public function actionIndex()
+    public function actionLogin()
     {
-        echo 'UsersControllers -> actionIndex';
+        if ($this->isPost){
+            //echo $this->post->password; // or all instead of login
+            $user = Users::FindByLoginAndPassword($this->post->login, $this->post->password);
+            if(!empty($user)){
+                Users::LoginUser($user);
+                return $this->redirect('/');
+//                header('Location: /');
+//                die;
+            }else
+                $this->template->setParam('error_message', 'Неправильний логін та/або пароль');
+        }
+            return $this->render();
+    }
+
+    public function actionLogout(){
+        Users::LogoutUser();
+        return $this->redirect('/users/login');
     }
 }
