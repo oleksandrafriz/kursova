046e96a016bf85f3ae0bf325c30867e5ae52abb6
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-05-02 21:06:53 +0300

    added users/login.php

---
+++ cms/views/users/login.php
@@ -0,0 +1,22 @@
+<?php
+$this->Title = 'Вхід на сайт';
+?>
+
+
+<form method="post" action="">
+    <?php
+    if (!empty($error_message)) : ?>
+        <div class="alert alert-danger" role="alert">
+           <?=$error_message ?>
+        </div>
+    <?php endif; ?>
+    <div class="mb-3">
+        <label for="inputEmail" class="form-label">Логін/email</label>
+        <input name="login" type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+        <label for="inputPassword" class="form-label">Пароль</label>
+        <input name="password" type="password" class="form-control" id="inputPassword">
+    </div>
+    <button type="submit" class="btn btn-primary">Увійти</button>
+</form>
