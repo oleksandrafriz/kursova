5717fbe3cf107be47488d4d8286091cf068a1987
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-05-02 21:03:35 +0300

    added changes for Controller

--- cms/core/Controller.php
+++ cms/core/Controller.php
@@ -5,16 +5,42 @@ namespace core;
 class Controller
 {
     protected $template;
+    public $isPost = false;
+    public $isGet = false;
+    public $post;
+    public $get;
+
     public function __construct(){
         $action = Core::get()->actionName;
         $module = Core::get()->moduleName;
         $path = "views/{$module}/{$action}.php";
         $this->template = new Template($path);
+        switch ($_SERVER['REQUEST_METHOD']) {
+            case 'POST':
+                $this->isPost = true;
+            break;
+            case 'GET':
+                $this->isGet = true;
+                break;
+        }
+
+        $this->post = new Post();
+        $this->get = new Get();
     }
 
-    public function render(){
+    public function render($pathToView = null){
+        if($pathToView != null){
+            $this->template->setTemplateFilePath($pathToView);
+        }
+
         return[
             'Content' => $this->template->getHTML()
         ];
+    }
+
+    public function redirect($path)
+    {
+        header("Location: {$path}");
+        die;
     }
 }
