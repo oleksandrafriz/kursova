e280c949d678d2577e0fabcccee4984e55511865
Oleksandra Friz <ipz221_foi@student.ztu.edu.ua>
2024-05-02 21:04:08 +0300

    added changes for Model

--- cms/core/Model.php
+++ cms/core/Model.php
@@ -2,34 +2,71 @@
 
 namespace core;
 
+use models\News;
+
 class Model
 {
     protected $fieldsArray;
-    protected $primaryKey = 'id';
+    protected static $primaryKey = 'id';
+    protected static $tableName = '';
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->fieldsArray = [];
     }
 
-    public function __set($name, $value){
-    $this->fieldsArray[$name] = $value;
+    public function __set($name, $value)
+    {
+        $this->fieldsArray[$name] = $value;
     }
 
-    public function __get($name){
-    return $this->fieldsArray[$name];
+    public function __get($name)
+    {
+        return $this->fieldsArray[$name];
+    }
+
+    public static function deleteById($id)
+    {
+        Core::get()->db->delete(static::$tableName, [static::$primaryKey => $id]);
+    }
+
+    public static function deleteByCondition($conditionAssocArray)
+    {
+        Core::get()->db->delete(static::$tableName, $conditionAssocArray);
+    }
+
+    public static function findById($id)
+    {
+        $arr = Core::get()->db->select(static::$tableName, '*', [static::$primaryKey => $id]);
+        if (count($arr) > 0) {
+            return $arr[0];
+        } else {
+            return null;
+        }
+    }
+
+    public static function findByCondition($conditionAssocArray)
+    {
+        $arr = Core::get()->db->select(static::$tableName, '*', $conditionAssocArray);
+        if (count($arr) > 0) {
+            return $arr;
+        } else {
+            return null;
+        }
+
     }
 
     public function save()
     {
-        $value = $this->{$this->primaryKey};
+        $value = $this->{static::$primaryKey};
         if (empty($value)) {
             //insert
-            Core::get()->db->insert($this->table, $this->fieldsArray);
+            Core::get()->db->insert(static::$tableName, $this->fieldsArray);
         } else {
             //update
-            Core::get()->db->update($this->table, $this->fieldsArray,
+            Core::get()->db->update(static::$tableName, $this->fieldsArray,
                 [
-                    $this->primaryKey => $this->{$this->primaryKey}
+                    static::$primaryKey => $this->{static::$primaryKey}
                 ]);
         }
     }
