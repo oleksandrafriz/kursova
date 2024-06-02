245a6a0377ad63d7a480c629d899b23a718b6f35
oleksandrafriz <ipz221_foi@student.ztu.edu.ua>
2024-06-02 22:22:32 +0300

    added changes for index files

--- cms/views/layouts/index.php
+++ cms/views/layouts/index.php
@@ -1,13 +1,12 @@
 <?php
 /** @var string $Title */
 /** @var string $Content */
-if(empty($Title))
+if (empty($Title))
     $Title = '';
 
-if(empty($Content))
+if (empty($Content))
     $Content = '';
 ?>
-
 
 <!doctype html>
 <html lang="en">
@@ -22,50 +21,105 @@ if(empty($Content))
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
             crossorigin="anonymous"></script>
+    <style>
+        footer {
+            background-color: #f8f9fa;
+            color: #343a40;
+            padding: 40px 0;
+        }
+        footer .nav-link {
+            color: #343a40;
+        }
+        footer .social-icons a {
+            color: #343a40;
+            margin: 0 10px;
+            font-size: 24px;
+        }
+        footer .contact-info p {
+            margin: 0;
+        }
+        .header-image {
+            width: 100%;
+            height: 400px;
+            object-fit: cover;
+            display: block;
+            border-radius: 4px;
+        }
+        .navigation {
+            background-color: #fff;
+            padding: 10px 0;
+        }
+        .navigation a {
+            font-weight: bold;
+        }
+        .navigation a:hover {
+            text-decoration: underline;
+        }
+
+        .text-decoration-none{
+            color: #333;
+        }
+
+        .navigation .logo {
+            margin: 0 20px;
+        }
+
+        .navigation .logo img {
+            height: 50px;
+        }
+        .navigation .menu-left,
+        .navigation .menu-right {
+            display: flex;
+            align-items: center;
+        }
+        .navigation .menu-left {
+            margin-right: auto;
+        }
+
+        .navigation .menu-right {
+            margin-left: auto;
+        }
+
+    </style>
 </head>
 <body>
 <div class="container">
     <header class="p-3 mb-3 border-bottom">
-        <div class="container">
-            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                        <use xlink:href="#bootstrap"></use>
-                    </svg>
-                </a>
-
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="/" class="nav-link px-2 link-secondary">Головна</a></li>
-                    <li><a href="/products" class="nav-link px-2 link-body-emphasis">Усі прикраси</a></li>
-                    <li><a href="/categories" class="nav-link px-2 link-body-emphasis">Категорії</a></li>
-                    <li><a href="/stones" class="nav-link px-2 link-body-emphasis">Каталог каменів</a></li>
-                    <?php if(!\models\Users::IsUserLogged()) : ?>
-                    <li><a href="/users/login" class="nav-link px-2 link-body-emphasis">Увійти</a></li>
-<!--                        <li><a href="/users/register" class="nav-link px-2 link-body-emphasis">Зареєструватись</a></li>-->
-                    <?php endif; ?>
-                </ul>
-
-                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                    <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-                </form>
-                <?php if(\models\Users::IsUserLogged()) : ?>
-                <div class="dropdown text-end">
-                    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+        <div class="container navigation">
+            <div class="d-flex align-items-center justify-content-between">
+                <div class="logo">
+                    <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+                        <img src="/logo.png" alt="Логотип">
                     </a>
+                </div>
 
-                    <ul class="dropdown-menu text-small">
-                        <li><a class="dropdown-item" href="#">New project...</a></li>
-                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-<!--                        <li>-->
-<!--                            <hr class="dropdown-divider">-->
-<!--                        </li>-->
-                        <li><a class="dropdown-item" href="/users/logout">Вийти</a></li>
+                <div class="menu-links">
+                    <ul class="nav mb-2 mb-md-0">
+                        <li><a href="/" class="nav-link px-2 link-secondary">Головна</a></li>
+                        <li><a href="/products" class="nav-link px-2 link-body-emphasis">Усі прикраси</a></li>
+                        <li><a href="/categories" class="nav-link px-2 link-body-emphasis">Категорії</a></li>
+                        <li><a href="/stones" class="nav-link px-2 link-body-emphasis">Каталог каменів</a></li>
+                        <li><a href="/about" class="nav-link px-2 link-body-emphasis">Про нас</a></li>
                     </ul>
                 </div>
-                <?php endif; ?>
+
+                <div class="user-menu">
+                    <?php if(!\models\Users::IsUserLogged()) : ?>
+                        <a href="/users/login" class="nav-link px-2 link-body-emphasis"><img src="lock.png" alt=""></a>
+                    <?php endif; ?>
+                    <?php if(\models\Users::IsUserLogged()) : ?>
+                        <div class="dropdown text-end">
+                            <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
+                               data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                            </a>
+
+                            <ul class="dropdown-menu text-small">
+                                <li><a class="dropdown-item" href="/users/logout">Вийти</a></li>
+                            </ul>
+                        </div>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
     </header>
@@ -75,22 +129,33 @@ if(empty($Content))
         <?= $Content?>
     </div>
 
-    <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Головна</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Каталог каменів</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Про нас</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Умови роботи</a></li>
-        </ul>
-        <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
-        <div class="d-flex justify-content-center">
-            <a href="#" class="text-body-secondary mx-2"><img src="instagram_icon.png" alt="Instagram"></a>
-            <a href="#" class="text-body-secondary mx-2"><img src="facebook_icon.png" alt="Facebook"></a>
-            <a href="#" class="text-body-secondary mx-2"><img src="etsy_icon.png" alt="Etsy"></a>
-        </div>
-        <div class="text-center text-body-secondary mt-3">
-            <p>Контакти: +380 (99) 48 78 049 | workshop.form.a@gmail.com</p>
-            <p>Години роботи: Прийом замовлень: на сайті цілодобово | Обробка замовлень: 10:00-17:00 | Виготовлення: Пн-Сб 10:00 - 17:00</p>
+    <footer class="py-4 mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <h5>Контакти</h5>
+                    <p>+380 (99) 48 78 049</p>
+                    <p><a href="mailto:workshop.form.a@gmail.com" class="text-decoration-none">bijou@gmail.com</a></p>
+                </div>
+                <div class="col-md-4 text-center">
+                    <h5>Години роботи</h5>
+                    <p>Прийом замовлень: на сайті цілодобово</p>
+                    <p>Опрацювання замовлень: 10:00-17:00</p>
+                    <p>Виготовлення: Пн-Сб 10:00 - 17:00</p>
+                </div>
+                <div class="col-md-4 text-center">
+                    <h5>Розділи сайту</h5>
+                    <p><a href="/stones" class="text-decoration-none">Каталог каменів</a></p>
+                    <p><a href="/about" class="text-decoration-none">Про нас</a></p>
+                    <p><a href="#" class="text-decoration-none">Умови роботи</a></p>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center social-icons mt-3">
+                <a href="#"><img src="/inst.png" alt="Instagram" width="24" height="24"></a>
+                <a href="#"><img src="/facebook.png" alt="Facebook" width="24" height="24"></a>
+                <a href="#"><img src="/telegram.png" alt="Etsy" width="24" height="24"></a>
+            </div>
+            <p class="text-center mt-3">© 2024 Oleksandra Friz</p>
         </div>
     </footer>
 </div>
--- cms/views/site/index.php
+++ cms/views/site/index.php
@@ -2,16 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cover Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/>
     <style>
         body, html {
             height: 100%;
             margin: 0;
             color: #333;
-            background: #fff;
+            background: #f5f5f5;
+            font-family: Arial, sans-serif;
         }
         .cover-container {
             height: 100%;
@@ -20,9 +22,30 @@
             align-items: center;
             justify-content: center;
             text-align: center;
+            background: #AEC09A;
+            color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            margin-top: 50px;
         }
-        .cover-container h1, .cover-container p {
+        .cover-container h1 {
+            font-size: 2.5em;
             margin: 0.4em 0;
+        }
+        .cover-container p {
+            background: rgba(255, 255, 255, 0.2);
+            padding: 15px;
+            border-radius: 8px;
+            margin: 0.4em 0;
+            margin-bottom: 24px;
+        }
+        .cover-container .btn-primary {
+            background: #ff6f61;
+            border-color: #ff6f61;
+            padding: 10px 20px;
+            font-size: 1.2em;
+            margin-top: 1em;
         }
         .nav {
             background: none;
@@ -30,14 +53,93 @@
         .nav-link {
             color: #fff;
         }
+
+        a{
+            color: #333;
+        }
+
+        .stones_link {
+            color: #fff;
+            background-color: #344C11;
+            padding: 12px 8px;
+            border-radius: 6px;
+            text-decoration: none;
+            margin-top: 5px;
+        }
+
+        .main_text {
+            width: 100%;
+            max-width: 600px;
+        }
+
+        @media (max-width: 768px) {
+            .cover-container {
+                padding: 15px;
+                margin-top: 20px;
+            }
+            .cover-container h1 {
+                font-size: 2em;
+            }
+            .cover-container p {
+                font-size: 1em;
+                padding: 10px;
+            }
+            .cover-container .btn-primary {
+                font-size: 1em;
+                padding: 8px 16px;
+            }
+        }
+        @media (max-width: 576px) {
+            .cover-container h1 {
+                font-size: 1.5em;
+            }
+            .cover-container p {
+                font-size: 0.9em;
+                padding: 8px;
+            }
+            .cover-container .btn-primary {
+                font-size: 0.9em;
+                padding: 6px 12px;
+            }
+        }
     </style>
 </head>
 <body>
 
-<div class="cover-container">
-    <h1>Cover your page.</h1>
-    <p>Cover is a one-page template for building simple and beautiful home pages.</p>
-    <button class="btn btn-primary">Learn more</button>
+
+<div class="header-slider">
+    <div>
+        <img src="back.jpg" alt="Header Image 1" class="header-image">
+    </div>
+    <div>
+        <img src="back_1.jpg" alt="Header Image 2" class="header-image">
+    </div>
+    <div>
+        <img src="back_2.jpg" alt="Header Image 3" class="header-image">
+    </div>
 </div>
+
+<div class="cover-container">
+    <h1>Сайт - побудований у вигляді каталогу</h1>
+    <p class="main_text">Для замовлення Вам потрібно <br>
+        підібрати форму прикраси - з відповідного розділу видів прикрас - та камінь з Каталогу каменів .<br>
+        Зателефонувати за номером <span style="color: #051b11; font-weight: bold">+380 (99) 48 78 049</span> щоб менеджер оформив ваше замовлення.
+    </p>
+
+    <p class="main_text">І все - прикраса буде створена за 1 день! У будь-якому випадку - менеджер напише Вам для уточнення деталей.</p>
+    <button class="nav-link px-2 link-body-emphasis"><a href="/stones" class="stones_link">Каталог каменів</a></button>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $('.header-slider').slick({
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+    });
+</script>
+
 </body>
 </html>
